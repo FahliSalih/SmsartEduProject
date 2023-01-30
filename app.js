f28c5e69277fa37a64e1160b4a16103aadc2ec03
bosch loreyn <boschloreyn@hotmail.com>
2023-01-30 14:46:19 +0300

    lesson 02

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app= express();
 
+//template Engine
+app.set("view engine", "ejs");
 
+//middlewares
+app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
-    res.status(200).send('İNDEX SAYFASI');
+    res.status(200).render('index',{
+      page_name: "index"
+    });
+});
+
+app.get("/about", (req,res)=>{
+  res.status(200).render('about',{
+    page_name: "about"
+  });
 });
 
 const port = 5000;
